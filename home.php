959ea8c7ea7f25312beeb1a1e3ba4989acf6a11c
Elton <eltonknoxville@hotmail.com>
2013-12-19 00:23:15 -0400

    Iniciando Projeto
    Iniciando Projeto

---
+++ home.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link href="style.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<table width="976" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td width="726" valign="top"><div class="quadro"><span class="titulo">
+      <div class="borda3"><a href="?pg=quemsomos.php"><img src="images/img.jpg" width="214" height="101" border="0" /></a></div>
+      Nome da Empresa</span><br />
+      <br />
+      <span class="txt2">Apresenta&ccedil;&atilde;o</span><br />
+      Este espa&ccedil;o vai um pequeno texto de chamada da apresenta&ccedil;&atilde;o da Empresa, linkado com uma pagina interna personalizada incluindo textos institucionais como tamb&eacute;m. Apresenta&ccedil;&atilde;o, Empresa, Miss&atilde;o, Valores, Hist&oacute;ria e etc...
+      <div>
+        <div align="right"><a href="?pg=quemsomos.php" class="link">Veja mais</a></div>
+      </div>
+    </div>
+        <br />
+        <div style="width:465px; float:left; margin-right:30px"><span class="titulo">Ciclo Produtivo</span><br />
+            <br />
+            <div class="quadro1"><img src="images/ciclo.jpg" width="465" height="212" />
+                <div class="botao"></div>
+              <div class="fundosect">
+                <div class="sectbanner"></div>
+                <div class="sectbanner2"></div>
+                <div class="sectbanner"></div>
+                <div class="sectbanner"></div>
+                <div class="sectbanner"></div>
+                <div class="sectbanner"></div>
+              </div>
+              <div class="botao2"></div>
+              <br />
+                <br />
+                <br />
+                <br />
+                <span class="txt2">Conhe&ccedil;a os cliclos produtivo da Borracha, Descri&ccedil;&atilde;o do servi&ccedil;o que vai ficar em destaque na principal do site, com um link para se&ccedil;&atilde;o interna com o conteudo completo referente a mesma...</span><br />
+                <br />
+                <span class="txt2">1&ordm; Titulo do Ciclo</span><br />
+              Descri&ccedil;&atilde;o do servi&ccedil;o que vai ficar em destaque na principal do site, com um link para se&ccedil;&atilde;o interna com o conteudo completo referente a mesma...<br />
+              <div class="pontilhado"></div>
+              <span class="txt2">2&ordm; Titulo do Ciclo</span><br />
+              Descri&ccedil;&atilde;o do servi&ccedil;o que vai ficar em destaque na principal do site, com um link para se&ccedil;&atilde;o interna com o conteudo completo referente a mesma...<br />
+              <br />
+              <br />
+              <div>
+                <div align="right"><a href="?pg=cicloprodutivo.php" class="link">Veja mais</a></div>
+              </div>
+            </div>
+        </div>
+      <div class="quadro2"><span class="titulo">Links &Uacute;teis</span><br />
+            <br />
+            <span class="txt2">Nome da Empresa</span> <br />
+            <a href="http://www.winnerdespachos.com.br" target="_blank" class="link2">&gt; www.winnerdespachos.com.br</a><br />
+            </span>
+            <div class="pontilhado2"></div>
+        <span class="txt2">Nome da Empresa </span><br />
+            <a href="http://www.winnerdespachos.com.br" target="_blank" class="link2">&gt; www.winnerdespachos.com.br</a><br />
+            <div class="pontilhado2"></div>
+        <span class="txt2">Nome da Empresa </span><br />
+            <a href="http://www.winnerdespachos.com.br" target="_blank" class="link2">&gt; www.winnerdespachos.com.br</a>
+            <div class="pontilhado2"></div>
+        <span class="txt2">Nome da Empresa</span> <br />
+            <a href="http://www.winnerdespachos.com.br" target="_blank" class="link2">&gt; www.winnerdespachos.com.br</a>
+            <div class="pontilhado2"></div>
+        <span class="txt2">Nome da Empresa</span> <br />
+            <a href="http://www.winnerdespachos.com.br" target="_blank" class="link2">&gt; www.winnerdespachos.com.br</a><br />
+            <br />
+            <div>
+              <div align="right"><a href="?pg=links.php" class="link">Veja mais</a></div>
+            </div>
+      </div>
+      <br />
+        <br />
+        <div class="quadro2"><span class="titulo">Sobre a Borracha</span><br />
+            <br />
+            <span class="txt2">Fique por dentro!</span><br />
+            <br />
+            <div class="borda2"><a href="?pg=sobreborracha.php"><img src="images/sobreborracha.jpg" width="212" height="79" border="0"/></a></div>
+          <p>&nbsp;</p>
+          Este espa&ccedil;o vai um pequeno texto de chamada dada a apresenta&ccedil;&atilde;o da Empresa, linkado com uma pagina...
+          <div>
+              <div align="right"><a href="?pg=sobreborracha.php" class="link">Veja mais</a></div>
+          </div>
+        </div></td>
+    <td width="250" valign="top"><div class="titulo" style="margin-left:30px">Parceiros</div>
+        <br />
+        <div class="quadro12">
+          <div class="selecao"><a href="javascript:void(0);"><img src="images/parceiro3.gif" width="190" height="102" border="0"/></a></div>
+          <div class="selecao"><a href="javascript:void(0);"><img src="images/parceiro2.gif" width="190" height="102" border="0"/></a></div>
+          <div class="selecao"><a href="javascript:void(0);"><img src="images/parceiro1.gif" width="190" height="102" border="0"/></a></div>
+        </div>
+      <div class="quadro13"><a href="?pg=parceiros.php" class="link3">Veja Mais</a> <img src="images/setas.gif" /></div>
+      <div style="margin-left:30px">
+      <div class="borda2"><a href="?pg=contatos.php"><img src="images/contatos.jpg" width="214" height="243" border="0"/></a></div></td>
+  </tr>
+</table>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link href="style.css" rel="stylesheet" type="text/css"/>
+<title>BF Borracha da Floresta</title>
+<script type="text/javascript">
+<!--
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+//-->
+</script>
+</head>
+
+<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" onload="MM_preloadImages('images/idealive2.png','images/menu.png')">
+<table width="976" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td><div style="height:29px; margin-top:10px"><span class="txt6">hoje, 
+      <?php
+$dia = date("d");
+$mes = date("F"); 
+$ano = date("Y"); 
+		if($mes == 'January'){
+		$mes = 'Janeiro';}		
+		elseif($mes == 'February'){
+		$mes = 'Fevereiro';}	
+		elseif($mes == 'March'){
+		$mes = 'Mar&ccedil;o';}	
+		elseif($mes == 'April'){
+		$mes = 'Abril';}		
+		elseif($mes == 'May'){
+		$mes = 'Maio';}		
+		elseif($mes == 'June'){
+		$mes = 'Junho';}	
+		elseif($mes == 'July'){
+		$mes = 'Julho';}	
+		elseif($mes == 'August'){
+		$mes = 'Agosto';}		
+		elseif($mes == 'September'){
+		$mes = 'Setembro';}		
+		elseif($mes == 'October'){
+		$mes = 'Outubro';}		
+		elseif($mes == 'November'){
+		$mes = 'Novembro';}		
+		elseif($mes == 'December'){
+		$mes = 'Dezembro';}			
+		$data = "$dia de $mes de $ano";		
+		echo "$data";
+?>
+    </span>
+     <div class="selecao2"><a href="javascript:void(0);"><img src="images/usa.png" width="32" height="24" border="0" /></a></div>
+    <div class="selecao2"><a href="javascript:void(0);"><img src="images/br.png" width="32" height="24" border="0" /></a></div>
+    <span style="float:right" class="txt6">Idiomas:</span>
+    </div>
+    <div class="borda"><img src="images/logo.jpg" width="255" height="221" align="left" />
+    <img src="images/banner.jpg" width="715" height="221" /></div>
+    <div style="float:left">
+      <a href="?pg=home.php"><div class="menulink">Principal</div></a>
+      <a href="?pg=quemsomos.php"><div class="menulink">Quem Somos</div></a>
+      <a href="?pg=sobreborracha.php"><div class="menulink">Sobre a Borracha</div></a>
+      <a href="?pg=cicloprodutivo.php"><div class="menulink">Ciclo Produtivo</div></a>
+      <a href="?pg=parceiros.php"><div class="menulink">Parceiros</div></a>
+      <a href="?pg=localizacao.php"><div class="menulink">Localiza&ccedil;&atilde;o</div></a>
+    <a href="?pg=contatos.php"><div class="menulink">Contatos</div></a>    </div></td>
+  </tr>
+  <tr>
+    <td height="30">&nbsp;</td>
+  </tr>
+</table>
+<?php      
+  /**  miolo **/ 
+	$pg=isset($_GET["pg"])?$_GET["pg"]:"home.php";
+	$pg=addslashes($pg);	
+	//Verifica se a string passada possui algum trecho inv&aacute;lido
+    //Caso tenha mostra uma mensagem de erro
+    if(eregi("http|www|ftp|.dat|.txt|.gif|wget| |.asp|/|\\\\", $pg))
+    {    	
+        //include("home.php");
+        echo "<script type=\"text/Javascript\">";
+        echo "location='index.php'";
+        echo "</script>";
+        return;
+    //Se a vari&aacute;vel passada estiver dentro das normas, executa o else abaixo
+    }else{
+        if(!empty($pg) && file_exists($pg)) {
+            @include ($pg);
+        }else{
+            @include ("home.php"); //essa seria a sua p&aacute;gina principal
+        }
+    }
+?>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td height="30" valign="top">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="213" valign="top" background="images/funbase.gif"><table width="970" border="0" align="center" cellpadding="0" cellspacing="0">
+      <tr>
+        <td height="62" colspan="2" align="right">
+        <div class="selecao2"><a href="javascript:void(0);"><img src="images/orkut.png" width="41" height="42" border="0" align="absmiddle" /></a></div>
+        <div class="selecao2"><a href="javascript:void(0);"><img src="images/twitter.png" width="41" height="42" hspace="8" border="0" align="absmiddle" /></a></div>
+        <div class="selecao2"><a href="javascript:void(0);"><img src="images/facebook.png" width="41" height="42" border="0" align="absmiddle" /></a></div>
+        <span style="float:right; margin-top:20px" class="titulo5">Redes Sociais: </span>        </td>
+        </tr>
+      <tr>
+        <td width="50%"><p class="txt5"><strong>BF Borracha da Floresta</strong></p>
+          <p class="txt5">Endere&ccedil;o: Av. nomedarua n&ordm;1234 - Centro - Manaus-AM</p>
+          <p class="txt5">Fone: (92) 1234.1234 - 1234.5678</p>
+          <p class="txt5">&copy; 2011 - Copyright - Todos os direitos reservados</p></td>
+        <td width="50%" align="right" valign="bottom"><a href="http://www.idealive.com.br" target="_blank"><img src="images/idealive.png" width="99" height="34" border="0" align="absmiddle" / title="Desenvolvimento" onmouseover="this.src='images/idealive2.png'" onmouseout="this.src='images/idealive.png'"></a></td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+</body>
+</html>
